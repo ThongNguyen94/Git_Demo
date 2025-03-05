e3de7e9f41085f15f130b32e9bf5cbe0e145725a
ThongNguyen94 <Nineswordsofdugu@gmail.com>
2025-03-05 19:31:40 +0100

    add new main1 into branch

---
+++ main1.cpp
@@ -0,0 +1,8 @@
+// Your First C++ Program
+#include <iostream>
+// Add 1 more line to test GIT
+// New main cpp in branch git and try to push it also in github
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
